5e9b7b9ae4c2294c520c4e5b86b83a9f50b06ab8
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-12 08:12:38 -0500

    Set up multiple routes.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<p>boiler</p>, document.getElementById('app'))
+const ExpenseDashboardPage = () => <div>dashboard component!</div>
+const AddExpensePage = () => <div>AddExpensePage component!</div>
+const EditExpensePage = () => <div>EditExpensePage component!</div>
+const HelpPage = () => <div>HelpPage component!</div>
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
