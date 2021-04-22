a74c69254b6fc9587e1ba20dab1b4d8066e04aec
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-22 13:59:23 -0500

    Add tests w/ spies to the ExpenseForm

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import moment from 'moment'
 // there's no connection to redux, so we can just get it
 import ExpenseForm from '../../components/ExpenseForm'
 import expenses from '../fixtures/expenses'
@@ -77,7 +78,6 @@ test('should set amount if valid input', () => {
   expect(wrapper.state('amount')).toBe(value)
 })
 
-// 12.122
 test('should not set amount if invalid input', () => {
   const value = '12.122'
   const wrapper = shallow(<ExpenseForm />)
@@ -85,4 +85,78 @@ test('should not set amount if invalid input', () => {
     target: { value }
   })
   expect(wrapper.state('amount')).toBe('')
+})
+
+// working with spies - to create fake functions created by jest for us, and we can make assertions about them, like:
+// @ https://jestjs.io/docs/expect
+// @ https://jestjs.io/docs/expect#tohavebeencalled
+// - .toHaveBeenCalled() will throw an error if the spy was never called and pass if it was called
+// @ https://jestjs.io/docs/expect#tohavebeencalledtimes
+// -- function was called and called w/ the right stuff
+// @ https://jestjs.io/docs/expect#tohavebeencalledwith
+// @ https://jestjs.io/docs/expect#tohavebeenlastcalledwith
+// -- not just called but called with the right stuff
+// - check if the fake function was called
+// - how many times it was called
+// - if called w/ specific args
+// SO
+// 1) CREATE FAKE FUNCTIONS
+// 2) PASS THEM INTO COMPONENTS
+// 3) MAKE SURE THEY WERE CALLED AS EXPECTED
+
+/* BASIC EXAMPLE 
+test('should call onSubmit prop for valid form submission', () => {
+  //! 1 - Create a new spy
+  const onSubmitSpy = jest.fn()
+  // - now we have access to a brand new set of assertions we can make
+  // call spy?
+  onSubmitSpy('Manny', 'The Keys')
+  //! 2 - Check that our spy was called
+  expect(onSubmitSpy).toHaveBeenCalledWith('Manny', 'The Keys')
+})
+*/
+// so we're making sure that the error gets cleared and the right stuff gets called
+test('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn()
+  //! 2 - Render the expense form, w/
+  //      - props (so we have real valid data)
+  //      - our event handler w/ the spy
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />)
+  //! 3 - now we can simulate the form submission
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  })
+  // - so now the form has been submitted and we can make assertions about what should have happened
+  expect(wrapper.state('error')).toBe('')
+  //! 4 - make sure it was called AND with the correct stuff
+  // - since it's a submission there's a id discreprancy, so we have to manually pass our desired object
+  expect(onSubmitSpy).toHaveBeenCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  })
+})
+
+test('should set new date on date change', () => {
+  const now = moment()
+  const wrapper = shallow(<ExpenseForm />)
+  // find that single date picker
+  // - we can actually find by component
+  // - find one of its props and call it
+  //   @ https://enzymejs.github.io/enzyme/docs/api/ShallowWrapper/prop.html
+  // - then call what comes back, the actual handler
+  //   - and pass in a new moment instance
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now)
+  // now we can make an assertion that the state was correctly set
+  expect(wrapper.state('createdAt')).toEqual(now)
+})
+
+test('should set calendar focus on change', () => {
+  const focused = true
+  const wrapper = shallow(<ExpenseForm />)
+  // make a change and pass it the object that it is expecting
+  wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused })
+  // now make sure the state was changed correctly
+  expect(wrapper.state('calendarFocused')).toEqual(focused)
 })
