ec51fad093b26440819c0b8751e22625ff582e38
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-04-22 14:55:13 -0500

    Add tests to AddExpensePage and use beforeEach() to manage recurring variable declarations

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,44 @@ import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = (props) => (
+// To avoid inline functions we'll turn this into a class
+/*const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
     <ExpenseForm
       onSubmit={(expense) => {
-        props.dispatch(addExpense(expense))
+        // props.dispatch(addExpense(expense))
+        // vv will make it easier to test
+        props.onSubmit(expense)
         props.history.push('/')
       }}
     />
   </div>
-)
+)*/
+// also export this class so we can import it into our tests!
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.onSubmit(expense)
+    this.props.history.push('/')
+  }
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm onSubmit={this.onSubmit} />
+      </div>
+    )
+  }
+}
 
-export default connect()(AddExpensePage)
+const mapDispatchToProps = (dispatch) => {
+  // goal is to return an object
+  // and we define various props, and these props are going to call dispatch
+  return {
+    onSubmit: (expense) => dispatch(addExpense(expense))
+  }
+}
+// - arg0 - is mapStateToProps - and we don't have any state, so undefined it is
+// - arg1 - is mapDispatchToProps - it's like mapStateToProps but instead of working w/ state it works w/ dispatch
+//             @ https://react-redux.js.org/using-react-redux/connect-mapdispatch
+export default connect(undefined, mapDispatchToProps)(AddExpensePage)
---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { AddExpensePage } from '../../components/AddExpensePage'
+import expenses from '../fixtures/expenses'
+
+//!!! TO REDUCE DUPLICATE CODE AND WRITE MORE ASSERTIONS USE JEST'S GLOBAL METHODS
+// @ https://jestjs.io/docs/api#beforeeachfn-timeout
+// - afterAll(fn, timeout) - runs after all the cases in a test file complete
+// - afterEach(fn, timeout) - runs after each test in a test file completes
+// - beforeAll(fn, timeout) - runs once before any of the tests run
+// - beforeEach(fn, timeout) - runs before each test case runs in a file
+
+//!!! so we'll set up the spies for the component and then each test case can just use those
+// - we want to define fresh copies before each test case
+let onSubmit, history, wrapper
+
+// @ https://jestjs.io/docs/api#beforeeachfn-timeout
+beforeEach(() => {
+  // these will run before each test case
+  onSubmit = jest.fn()
+  history = { push: jest.fn() }
+  wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+})
+//! ^^ now we can reuse these in all our test cases! 👻
+
+test('should render AddExpensePage correctly', () => {
+  //! 1) We'll set up some spies for the 2 things we'll pass into the component
+  //! 1a) vv We'll move these up to beforeEach()
+  // const onSubmit = jest.fn()
+  // const history = { push: jest.fn() }
+  // const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+  // now we can just take a snapshot of this and call it a day
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('should handle onSubmit', () => {
+  //! 1) We'll set up some spies for the 2 things we'll pass into the component
+  //! 1a) vv We'll move these up to beforeEach()
+  // const onSubmit = jest.fn()
+  // const history = { push: jest.fn() }
+  // const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+  //! 2) Now we'll actually call the onSubmit function with some fixture data
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+  //! 3) Make sure things were called with all the right stuff
+  // - spy #1 - history
+  expect(history.push).toHaveBeenLastCalledWith('/')
+  // - spy #2 - onSubmit - lastCalledWith the data we passed through
+  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+})
